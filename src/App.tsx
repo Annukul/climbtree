e83e44fb06751aaba0ecd457900bd94d3c669829
annukul-solidity <annukul.thakran@xcerpt.app>
2022-07-13 17:34:52 +0530

    Distance between current location and other location is calculated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { getDistance } from "geolib";
 
 function App() {
   const [longitude, setLongitude] = useState<any>();
   const [latitude, setLatitude] = useState<any>();
-  if (navigator.geolocation) {
-    console.log("Available");
-  } else {
-    console.log("Not Available");
-  }
+  const [distance, setDistance] = useState<any>();
 
   useEffect(() => {
     const error = (err: any) => {
@@ -16,25 +13,31 @@ function App() {
     };
 
     const options = {
-      enableHighAccuracy: false,
+      enableHighAccuracy: true,
       timeout: 15000,
       maximumAge: 0,
     };
 
     const success = (pos: any) => {
-      console.log(pos.coords.latitude, pos.coords.longitude);
+      const dist = getDistance({latitude: pos.coords.latitude, longitude: pos.coords.longitude}, {
+        latitude: 28.6519500,
+        longitude: 77.2314900,
+      });
       setLongitude(pos.coords.longitude);
       setLatitude(pos.coords.latitude);
+      setDistance(dist);
     };
-
-    navigator.geolocation.watchPosition(success, error, options);
-  }, [longitude, latitude]);
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>ClimbTree</h2>
-        {longitude && latitude && (
+        {distance && (
+          <p>You are {distance/1000} Kms away from Delhi.</p>
+        )}
+        {longitude && latitude ? (
           <a
             href={`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`}
             target="_blank"
@@ -42,6 +45,17 @@ function App() {
           >
             Click here
           </a>
+        ) : (
+          <>
+            <img
+              src="https://loading.io/icon/gfoopo"
+              width="10%"
+              height="10%"
+              style={{ position: "absolute" }}
+              className="giphy-embed"
+              alt="Loader"
+            />
+          </>
         )}
       </header>
     </div>
